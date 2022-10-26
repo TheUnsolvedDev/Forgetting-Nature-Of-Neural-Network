37f6118f6cb58ca1516f07894fd85b4ce2cb4760
Shuvrajeet Das <shuvrajeet17@gmail.com>
2022-10-26 23:31:27 +0530

    Update model.py

--- dont_forget/model.py
+++ dont_forget/model.py
@@ -9,26 +9,6 @@ metrics = [
 
 
 def lenet5():
-    # input = tf.keras.Input(shape=(28, 28, 1))
-    # conv1 = tf.keras.layers.Conv2D(filters=32, kernel_size=5,activation = 'tanh', padding='same')(input)
-    # maxpool2 = tf.keras.layers.MaxPool2D(
-    #     pool_size=(2, 2), strides=None, padding='valid')(conv1)
-    # droupout1 = tf.keras.layers.Dropout(0.25)(maxpool2)
-
-    # conv3 = tf.keras.layers.Conv2D(filters=64, kernel_size=5,activation = 'tanh', padding='valid')(maxpool2)
-    # maxpool3 = tf.keras.layers.MaxPool2D(
-    #     pool_size=(2, 2), strides=None, padding='valid')(conv3)
-    # dropout2 = tf.keras.layers.Dropout(0.25)(maxpool3)
-
-    # flat = tf.keras.layers.Flatten()(dropout2)
-    # fc1 = tf.keras.layers.Dense(units=240, activation='tanh')(flat)
-    # fc2 = tf.keras.layers.Dense(units=128, activation='tanh')(fc1)
-    # final = tf.keras.layers.Dense(units=10)(fc2)
-
-    # model = tf.keras.Model(inputs=input, outputs=final)
-
-    # return model
-
     inputs = tf.keras.layers.Input(shape=(28, 28))
     flat = tf.keras.layers.Flatten()(inputs)
     flat = tf.keras.layers.Dense(240, activation='tanh')(flat)
